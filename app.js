52eb1bb3962b2b198983e21fe4a692a005987c30
Gabriel <gabriel@multiverso.me>
2017-04-09 01:54:50 -0300

    Add getLikedPhotoList

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const tumblr = require('tumblr.js');
+const _ = require('lodash')
+const tumblr = require('tumblr.js')
 const client = tumblr.createClient({
-  consumer_key: process.env.TUMBLR_CONSUMER_KEY
-});
+  consumer_key: process.env.TUMBLR_CONSUMER_KEY,
+  consumer_secret:  process.env.TUMBLR_CONSUMER_SECRET,
+  token: process.env.TUMBLR_TOKEN,
+  token_secret: process.env.TUMBLR_TOKEN_SECRET
+})
 
 function getPhotoList(offset = 0) {
   client.blogPosts('1or1', { type: 'photo', limit: 20, offset: offset }, function (err, data) {
-    for (let post of data.posts) {
-      for (let photo of post.photos) {
-        console.log(photo.original_size.url);
-      }
-    }
-    if (offset <= (data.total_posts - 19)) getPhotoList(offset + 19);
-  });
+    _.forEach(data.posts, (post) => {
+      _.forEach(post.photos, (photo) => console.log(photo.original_size.url))
+    })
+    if (offset <= (data.total_posts - 19)) getPhotoList(offset + 19)
+  })
 }
 
-getPhotoList();
+function getLikedPhotoList(before) {
+  client.blogLikes('1or1', { before: before }, function (err, data) {
+    if (!data.liked_posts.length) return;
+
+    let posts = _.filter(data.liked_posts, {type: 'photo'})
+    let last = _.last(posts)
+
+    _.forEach(posts, (post) => {
+      _.forEach(post.photos, (photo) => console.log(photo.original_size.url))
+    })
+
+    getLikedPhotoList(last.liked_timestamp)
+  })
+}
+
+getLikedPhotoList(0)
